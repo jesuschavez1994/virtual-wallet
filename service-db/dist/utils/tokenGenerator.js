fa8d830ee88014238682956811daad2a22cd40fd
chavezjeg <chavezjeg@globalhitss.com>
2025-05-13 08:40:36 -0400

    chore: Excluir carpetas dist de service-client y service-db

--- service-db/dist/utils/tokenGenerator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateToken = void 0;
-const crypto_1 = require("crypto");
-const generateToken = () => {
-    const token = (0, crypto_1.randomInt)(100000, 999999).toString();
-    return token;
-};
-exports.generateToken = generateToken;
